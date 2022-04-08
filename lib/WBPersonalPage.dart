85b4195b64efce23c0f862e92b3dad1280abc3c0
wokebryant <wokeebryant@outlook.com>
2022-04-08 18:54:30 +0800

    [update] 和原生通信

--- lib/WBPersonalPage.dart
+++ lib/WBPersonalPage.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ja_flutter_module/utils/DeviceUtil.dart';
 
 /// WorkBox个人信息页面
 class WBPersonalPage extends StatelessWidget {
   const WBPersonalPage({Key? key}) : super(key: key);
 
+  static const platform = BasicMessageChannel("flutter/navigation", JSONMessageCodec());
+
   @override
   Widget build(BuildContext context) {
     DeviceUtil.setDarkStatusBar();
-    return _buildPersonalPage();
+    return _buildPersonalPage(context);
   }
 
-  Widget _buildPersonalPage() {
+  Widget _buildPersonalPage(BuildContext context) {
     /// 标题栏
     var title = ListTile(
       leading: const Icon(Icons.arrow_back_sharp),
       iconColor: Colors.black,
       title: Text(
         "个人信息", style: userInfoTextStyle(fontSize: 18)),
+      onTap: () => SystemNavigator.pop()
     );
 
     /// 用户等级，工资信息
@@ -62,7 +66,7 @@ class WBPersonalPage extends StatelessWidget {
     /// 提取工资按钮
     final extractPayRollBtn = Container(
       margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-      child: _buildButton("提取工资记录", () { })
+      child: _buildButton("提取工资记录", () => _jumpToWBExtractSalaryPage())
     );
 
     /// 工作明细按钮
@@ -102,6 +106,25 @@ class WBPersonalPage extends StatelessWidget {
     ),
     style: outlineButtonStyle
   );
+
+  /// 跳转WorkBox提取工资页面(Compose页面)
+  Future<void> _jumpToWBExtractSalaryPage() async {
+    Map<String, String> args = {
+      "route": "work_box_salary_record",
+      "2021-11-08 10:59": "6.9ACN",
+      "2021-07-30 1:55": "54.2736ACN",
+    };
+    Map<String, Object> map = {
+      "method": "navigation",
+      "args": args
+    };
+
+    final result = await platform.send(map) as List<dynamic>;
+    if (result.isNotEmpty && result[0] == "agree") {
+      // 接受Native返回值
+    }
+  }
+  
 }
 
 /// 按钮样式
@@ -132,3 +155,13 @@ TextStyle userInfoTextStyle({
     color: color,
     fontSize: fontSize
 );
+
+class SalaryRecord {
+  final String date;
+  final String salary;
+
+  const SalaryRecord({
+    required this.date,
+    required this.salary
+  });
+}
